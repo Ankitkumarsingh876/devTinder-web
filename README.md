57eeb91a0d4b99847a7f3a80a90edccaa146f2f0
Ankit Kumar Singh <singhankittomar.23@gmail.com>
2025-11-24 12:30:41 +0530

    Setup Redux

--- README.md
+++ README.md
@@ -16,5 +16,11 @@ second -2
 - install axios
 - CORS - install cors in backend => add middleware to win configuration : origin , credentials :
 - when you are making a API call so pass { withcredential : true}
+- configuration store = provider = createslice = add reducer to store
+- read all the documents of react redux toolkit
+- add redux toolkit in out chorome
+- login and see if your data is coming properly
+- navbars should update and user login
+- reafactors our code to to add constant file + create a components folder
 
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,21 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "./utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("donald@gmail.com");
   const [Password, setPassword] = useState("Donald123@");
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:7777/login",
+        BASE_URL + "/login",
         {
           email: emailId,
           password: Password,
@@ -17,7 +24,9 @@ const Login = () => {
           withCredentials: true,
         }
       );
-      console.log(res);
+      
+      dispatch(addUser(res.data));
+      return navigate("/");
     } catch (err) {
       console.log(err);
     }

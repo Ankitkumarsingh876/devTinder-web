6a596b8155d9b0a8e184cbe225964f4a04cdc1d7
Ankit Kumar Singh <singhankittomar.23@gmail.com>
2025-12-20 18:04:32 +0530

    Change backend URL link

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -14,7 +14,7 @@ const Feed = () => {
   const feedUser = async () => {
     if(feed) return;
     try{
-    const res = await axiosInstance.get(BASE_URL + "/profile/feed", {
+    const res = await axiosInstance.get(BASE_URL + "/feed", {
       withCredentials: true,
     })
     dispatch(addFeed(res.data));
